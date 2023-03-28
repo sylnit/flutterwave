8e4b30a45ccba48bf6e0903401edbc4c66bccedd
sylnit <h1samuel@yahoo.com>
2023-03-28 10:14:50 +0100

    Initial commit

---
+++ lib/flutterwave.ex
@@ -0,0 +1,18 @@
+defmodule Flutterwave do
+  @moduledoc """
+  Documentation for `Flutterwave`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Flutterwave.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/flutterwave_test.exs
@@ -0,0 +1,8 @@
+defmodule FlutterwaveTest do
+  use ExUnit.Case
+  doctest Flutterwave
+
+  test "greets the world" do
+    assert Flutterwave.hello() == :world
+  end
+end
